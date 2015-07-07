125d743be5863d9bb31539b2542aa9fba7f12521
shijq <shijq73@gmail.com>
2015-07-07 13:37:19 +0800

    Signed-off-by: shijq <shijq73@gmail.com>

--- ConnectorsInfoWnd.py
+++ ConnectorsInfoWnd.py
@@ -2,7 +2,6 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.uic import *
 
-
 class ConnectorsInfoWnd(QDialog):
   def __init__(self, parent, all_connectors):
     super(ConnectorsInfoWnd, self).__init__(parent)
@@ -10,7 +9,31 @@ class ConnectorsInfoWnd(QDialog):
     
     self.header = ['Label', 'Kind', 'Type'] 
     
-    model = ConnectorTableModel(self, all_connectors, self.header)
+    connectors = []
+    
+    for row in range(len(all_connectors)):      
+      connector_info = {}
+      connector_node = all_connectors[row]
+
+      label = ''
+      if('label' in connector_node.attrib):
+        label = connector_node.attrib["label"]   
+      connector_info['label'] = label
+      
+      kind = ''
+      if('connector-kind' in connector_node.attrib):
+        kind = connector_node.attrib["connector-kind"]   
+    
+      connector_info['kind'] = kind  
+      
+      type = ''
+      if('connector-type' in connector_node.attrib):
+        type = connector_node.attrib["connector-type"]
+      
+      connector_info['type'] = type  
+      connectors.append(connector_info)
+    
+    model = ConnectorTableModel(self, connectors, self.header)
     self.tableView.setModel(model)
 
     verticalHeader = self.tableView.verticalHeader();
@@ -19,37 +42,17 @@ class ConnectorsInfoWnd(QDialog):
     verticalHeader.setDefaultSectionSize(18);
 
     self.tableView.setItemDelegate(MyDelegate(self.tableView));
-    
-    for row in range(len(all_connectors)):      
-      
-      connector_node = all_connectors[row]
-
-      label = ''
-      if('label' in connector_node.attrib):
-        label = connector_node.attrib["label"]   
-      index = model.index(row, 0, QModelIndex())  
-      model.setData(index, label)
-  
-      kind = ''
-      if('connector-kind' in connector_node.attrib):
-        kind = connector_node.attrib["connector-kind"]   
-      index = model.index(row, 1, QModelIndex())  
-      model.setData(index, kind)      
-        
-      type = ''
-      if('connector-type' in connector_node.attrib):
-        type = connector_node.attrib["connector-type"]
-      index = model.index(row, 2, QModelIndex())    
-      model.setData(index, type) 
+    self.tableView.setEditTriggers(QAbstractItemView.AllEditTriggers)
       
 class ConnectorTableModel(QAbstractTableModel):    
-    def __init__(self, parent, all_connectors, header, *args):
+    def __init__(self, parent, connectors, header, *args):
         QAbstractTableModel.__init__(self, parent, *args)
-        self.all_connectors = all_connectors
+        self.connectors = connectors
+
         self.header = header
         
     def rowCount(self, parent):
-        return len(self.all_connectors)
+        return len(self.connectors)
         
     def columnCount(self, parent):
         return len(self.header)
@@ -62,8 +65,16 @@ class ConnectorTableModel(QAbstractTableModel):
       
     def setData(self, index, value, role=Qt.DisplayRole):
       if (index.isValid() and role == Qt.EditRole):
-        item = index.internalPointer()
-        item.setValue(value)
+        connector_info = self.connectors[index.row()]
+        if(index.column() == 0):
+          connector_info['label'] = value
+          
+        if(index.column() == 1):
+          connector_info['kind'] = value
+          
+        if(index.column() == 2):
+          connector_info['type'] = value
+        
         #emit dataChanged(index, index);
         return True;
       return False;
@@ -75,41 +86,28 @@ class ConnectorTableModel(QAbstractTableModel):
       elif role != Qt.DisplayRole:
           return None
           
-      data = index.model().data(index, Qt.EditRole);	
-      return data
-          
-    '''          
-      connector_node = self.all_connectors[index.row()]
-      
-  
+      connector_info = self.connectors[index.row()]
+
       if(index.column() == 0):
-        if('label' in connector_node.attrib):
-          return connector_node.attrib["label"]   
-        else:
-          return ''      
-      
+        return connector_info['label']
+        
       if(index.column() == 1):
-        if('connector-kind' in connector_node.attrib):
-          return connector_node.attrib["connector-kind"]   
-        else:
-          return ''
+        return connector_info['kind']
+        
       if(index.column() == 2):
-        if('connector-type' in connector_node.attrib):
-          return connector_node.attrib["connector-type"]   
-        else:
-          return ''        
+        return connector_info['type']       
 
           
-      return self.all_connectors[index.row()][index.column()]
-    '''
+      return None
+
     def headerData(self, col, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return self.header[col]
-            
-        if (role == Qt.TextAlignmentRole):
-           return Qt.AlignLeft | Qt.AlignVCenter  
-            
-        return None
+      if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+          return self.header[col]
+          
+      if (role == Qt.TextAlignmentRole):
+         return Qt.AlignLeft | Qt.AlignVCenter  
+          
+      return None
         
 class MyDelegate(QItemDelegate):
   def __inti__(self, parent):
@@ -127,7 +125,7 @@ class MyDelegate(QItemDelegate):
       
       if(index.column() == 2):
         combobox = QComboBox(parent)
-        combobox.addItems(['number', 'string'])
+        combobox.addItems(['boolean','number', 'string'])
         #combobox.currentIndexChanged[int].connect(self.currentIndexChanged)
         return combobox   
 
@@ -136,14 +134,19 @@ class MyDelegate(QItemDelegate):
  
   def setEditorData (self, editor, index):
 
+
     #self.m_finishedMapper.blockSignals(True);
-    data = index.model().data(index, Qt.EditRole);	
-    print(data)
-    
-    if(index.column() == 0):         
+    text = index.model().data(index, Qt.DisplayRole)    
+   
+    if index.column() == 0:
       QItemDelegate.setEditorData(self, editor, index);
  
-    if(index.column() == 1 or index.column() == 2):       
-      index = editor.findData(data)
-      editor.setCurrentIndex(index)
+    if index.column() == 1:
+      print(editor)
+      _ind = editor.findText(text)
+      print(_ind)
+      editor.setCurrentIndex(_ind)
     
+    if index.column() == 2:
+      _ind = editor.findText(text)
+      editor.setCurrentIndex(_ind)
